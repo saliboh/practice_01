5c043dce3b6d759a47772fc91db65b1cee8ebf0e
Andrean Earl <earl@nabepero.co.jp>
2022-03-09 19:30:40 +0800

    feat(build) update sh file

--- runme.sh
+++ runme.sh
@@ -2,3 +2,4 @@ cp src/.env.example src/.env
 docker-compose run --rm composer install
 docker-compose run --rm artisan key:generate
 docker-compose run --rm artisan migrate:fresh
+docker-compose run --rm artisan storage:link
